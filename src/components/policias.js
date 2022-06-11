a3a84963b3aeb3185c9fc279fc6e298805b3b033
Irvin Hdz <irvinphanto@gmail.com>
2022-06-11 18:25:44 -0500

    policias modificados

--- src/components/policias.js
+++ src/components/policias.js
@@ -5,20 +5,7 @@ function Policias() {
   return (
     <section className="secction2">
       <div className="oficial1">
-        <div className="Panel">
-          <header className="pannel1">
-            <h2><span>Oficila</span>Anna Ojdana</h2>
-            <div className="icon">
-              <svg>
-                <a href="https://www.flaticon.com/free-icons/shield" title="shield icons">Shield icons created by Ladalle CS - Flaticon</a>
-              </svg>
-            </div>
-          </header>
-            <p>Barely two months after graduating from the Police Academy, Officer Ojdana and her Field Training Officer answered a life-threatening domestic violence call. Not just for the victim, but for the officers as well.</p>
-            <div className="btn_info">
-              <a className="btn_info" href="#" Ver Su Historia></a>
-              <p> este es un arrafo para probar la ramas</p>
-            </div>
+        <div className="info-oficial">
         </div>
       </div>
     </section>
