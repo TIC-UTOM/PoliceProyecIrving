b4a6fda070b8d501ab70156017fa518f77d77037
Irvin Hdz <irvinphanto@gmail.com>
2022-06-11 19:21:34 -0500

    Divs sobre divs

--- src/components/policias.js
+++ src/components/policias.js
@@ -6,6 +6,14 @@ function Policias() {
     <section className="secction2">
       <div className="oficial1">
         <div className="info-oficial">
+          <h2>
+            <span>Official</span><br/>
+            Anna Ojdana
+          </h2>
+          <p>Barely two months after graduating from the Police Academy, Officer Ojdana and her Field Training Officer answered a life-threatening domestic violence call. Not just for the victim, but for the officers as well.</p>
+          <div>
+            <a>See Her Story</a>
+          </div>
         </div>
       </div>
     </section>
