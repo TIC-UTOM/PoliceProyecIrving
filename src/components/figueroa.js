bf221f6adf543bed18612db59468895d55adb0fb
Irvin Hdz <irvinphanto@gmail.com>
2022-07-13 01:42:09 -0500

    Nueva infrmacion

--- src/components/figueroa.js
+++ src/components/figueroa.js
@@ -1,87 +1,101 @@
-  import react from 'react';
-  import '../styles/figueroa.css';
-  import oficial1 from '../images/policia1.jpg'
-  import oficial2 from '../images/policia2.jpg'
-  import facebook from '../images/facebook.png'
-  import twitter from '../images/gorjeo.png'
+    import react from 'react';
+    import '../styles/figueroa.css';
+    import oficial1 from '../images/policia1.jpg'
+    import oficial2 from '../images/policia2.jpg'
+    import facebook from '../images/facebook.png'
+    import twitter from '../images/gorjeo.png'
 
-  function Policiafigueroa() {
-    return (
-      <div>
-        <div className='grid-container' banner-container>
-          <nav className="header-nav">
-            <ul class="nav justify-content-center">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="figueroa/*">Policias</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="tenencias/*">Tenencias</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Source</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Resourses</a>
-              </li>
-            </ul>
-          </nav>
+    function Policiafigueroa() {
+      return (
+        <div>
+          <div className='banner-container'>
+            <nav className="header-nav">
+              <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="figueroa/*">Policias</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="tenencias/*">Tenencias</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Source</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Resourses</a>
+                </li>
+              </ul>
+            </nav>
 
-              <div>
-                <h1 className='banner-titulo' >
-                  <span className='parallax'>
-                  <span>CONOCE</span>
-                  </span><br/>
-                  <span className='parallax'>
-                  <span>NUESTROS</span>
-                  </span><br/>
-                  <span className='parallaxx'>
-                  <span>POLICIAS</span>
-                  </span>
-                </h1>
-                  <div className='banner-texto'>
-                    <p>La Misión de la Policía Auxiliar es proteger la integridad física, los bienes, valores, mercancías de los particulares, empresarios, así como de las instituciones públicas y privadas de quienes contratan nuestros servicios dentro del límite territorial del estado de Michoacán, mediante los servicios de protección.</p>
-                  </div>
+                <div className='div-posicionado'>
+                  <h1 className='banner-title' >
+                    <span className='parallax'>
+                    <span>CONOCE </span>
+                    </span>
+                    <span className='parallax'>
+                    <span>NUESTROS </span>
+                    </span>
+                    <span className='parallaxx'>
+                    <span>POLICIAS</span>
+                    </span>
+                  </h1>
+                    <div className='banner-texto'>
+                      <p>La Misión de la Policía Auxiliar es proteger la integridad física, los bienes, valores, mercancías de los particulares, empresarios, así como de las instituciones públicas y privadas de quienes contratan nuestros servicios dentro del límite territorial del estado de Michoacán, mediante los servicios de protección.</p>
+                    </div>
+                </div>
+          </div>
+
+            <section id="pantalla-dividida">
+              <div className="derecha">
+              <h1><span class="creative">Official</span><br/>
+              Antonio Hernandez
+              </h1>
               </div>
-        </div>
-
-
-          <section id="pantalla-dividida">
-            <div className="derecha">
-            <h1><span class="creative">Official</span><br/>
-            Antonio Hernandez
-            </h1>
-            </div>
-            <div className="izquierda">
-              <p className="info">Apenas dos meses después de graduarse de la Academia de Policía, el official Hernandez y su Oficial de Entrenamiento de Campo respondieron una llamada de violencia doméstica que amenazaba su vida. No solo para la víctima, sino también para los oficiales.</p>
-              <div>
-                <button type="button" class="btn btn-outline-warning">Ver historia</button>
+              <div className="izquierda">
+                <p className="info">Apenas dos meses después de graduarse de la Academia de Policía, el official Hernandez y su Oficial de Entrenamiento de Campo respondieron una llamada de violencia doméstica que amenazaba su vida. No solo para la víctima, sino también para los oficiales.</p>
+                <div>
+                  <button type="button" class="btn btn-outline-warning">Ver historia</button>
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section id="pantalla-dividida">
-            <div className="izquierda"></div>
-            <div className="derecha">
-            <h1>
-              <span>Official</span><br/>
-              Leticia Lopez
-            </h1>
+            <section id="pantalla-dividida">
+              <div className="izquierda">
               <p>After finishing his shift, Officer Figueroa was on his way home when he noticed a man standing on an overpass bridge. Compelled to do the right thing, Officer Figueroa stopped to provide a helping hand.</p>
               <div>
-                <button type="button" class="btn btn-outline-warning">Ver historia</button>
+                  <button type="button" class="btn btn-outline-warning">Ver historia</button>
+                </div>
               </div>
-            </div>
-          </section>
+              <div className="derechas">
+              <h1>
+                <span class="creative">Official</span><br/>
+                Leticia Lopez
+              </h1>
+              </div>
+            </section>
 
-        <div className="footer">
-          <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
-          <a class="nav-link" href="/"><img src={facebook} alt="Logo Policia" height="30" width="30"/></a>
-          <a class="nav-link" href="/"><img src={twitter} alt="Logo Policia" height="30" width="30"/></a>
+            <section id="pantalla-dividida">
+              <div className="derecha">
+              <h1><span class="creative">Official</span><br/>
+              Antonio Hernandez
+              </h1>
+              </div>
+              <div className="izquierda">
+                <p className="info">Apenas dos meses después de graduarse de la Academia de Policía, el official Hernandez y su Oficial de Entrenamiento de Campo respondieron una llamada de violencia doméstica que amenazaba su vida. No solo para la víctima, sino también para los oficiales.</p>
+                <div>
+                  <button type="button" class="btn btn-outline-warning">Ver historia</button>
+                </div>
+              </div>
+            </section>
+
+          <div className="footer">
+            <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
+            <a class="nav-link" href="/"><img src={facebook} alt="Logo Policia" height="30" width="30"/></a>
+            <a class="nav-link" href="/"><img src={twitter} alt="Logo Policia" height="30" width="30"/></a>
           </div>
-      </div>
-    )
-  }
-  export default Policiafigueroa;
+        </div>
+      )
+    }
+    export default Policiafigueroa;
--- src/components/tenencias.js
+++ src/components/tenencias.js
@@ -6,79 +6,75 @@
 
   function Tenencias() {
       return (
-          <div>
-        <div className='grid-container' banner-container>
-        <nav className="header-nav">
-            <ul class="nav justify-content-center">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="figueroa">Policias</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Tenencias</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Source</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Resourses</a>
-              </li>
-            </ul>
-          </nav>
+        <div>
+          <div className='banner-container'>
+            <nav className="header-nav">
+              <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="figueroa/*">Policias</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="tenencias/*">Tenencias</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Source</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Resourses</a>
+                </li>
+              </ul>
+            </nav>
 
-            <div>
-              <h1 className='banner-titulo' >
-                <span className='parallax'>
-                <span>TENENCIAS</span>
-                </span><br/>
-                <span className='parallax'>
-                <span>DE</span>
-                </span><br/>
-                <span className='parallaxx'>
-                <span>MARAVATIO</span>
-                </span>
-              </h1>
-
-                <div className='banner-texto'>
-                  <p>Maravatío esta constituido una cabecera municipal, 109 encargados del orden y 10 tenencias  y 113 comunidades.</p>
+                <div className='div-posicionado'>
+                  <h1 className='banner-title' >
+                    <span className='parallax'>
+                    <span>TENECIAS </span>
+                    </span>
+                    <span className='parallax'>
+                    <span>DE </span>
+                    </span>
+                    <span className='parallaxx'>
+                    <span>MARAVATIO</span>
+                    </span>
+                  </h1>
+                    <div className='banner-texto'>
+                      <p>Maravatío esta constituido una cabecera municipal, 109 encargados del orden y 10 tenencias y 113 comunidades.</p>
+                    </div>
                 </div>
+          </div>
+
+        <div className="policia-hdz">
+            <img
+            src={tunga}
+            />
+            <div className="info-policiahdz">
+              <h2>
+                <span>Official</span><br/>
+                Antonio Hernandez
+              </h2>
+              <p>Apenas dos meses después de graduarse de la Academia de Policía, el official Hernandez y su Oficial de Entrenamiento de Campo respondieron una llamada de violencia doméstica que amenazaba su vida. No solo para la víctima, sino también para los oficiales.</p>
+              <div>
+                <button type="button" class="btn btn-outline-warning">Ver historia</button>
+              </div>
             </div>
+          </div>
 
-        </div>
-
-        <div className="container">
-          <section className="secction2">
-            <div className="oficial1">
-              <img
-              src={tunga}
-              />
-              <div className="info-oficial-ojdana">
-                <h2>San Pedro Tungareo</h2>
-                <p>su principal actividad económica es la agricultura.</p>
+            <div className="policia-lopez">
+              <img src={snmike} />
+              <div className="info-policialopez">
+                <h2>
+                <span>Official</span><br/>
+                Leticia Lopez
+                </h2>
+                <p>After finishing his shift, Officer Figueroa was on his way home when he noticed a man standing on an overpass bridge. Compelled to do the right thing, Officer Figueroa stopped to provide a helping hand.</p>
                 <div>
                   <button type="button" class="btn btn-outline-warning">Ver historia</button>
                 </div>
               </div>
             </div>
-          </section>
-
-          <div>
-            <section className="section3">
-              <div className="official2">
-                <img src={snmike} />
-                <div className="info-oficial">
-                  <h2>San Miguel Curahuango</h2>
-                  <p>Su principal actividad económica es la agricultura y las artesanías.</p>
-                  <div>
-                    <button type="button" class="btn btn-outline-warning">Ver historia</button>
-                  </div>
-                </div>
-              </div>
-            </section>
-          </div>
-        </div>
         <div className="footer">
           <a class="nav-link" href="/"><img src="https://laextra.mx/wp-content/uploads/2016/04/Polici%CC%81a-Federal-PF.png" alt="Logo Policia" height="150" width="200"/></a>
           <a class="nav-link" href="/"><img src={facebook} alt="Logo Policia" height="30" width="30"/></a>
